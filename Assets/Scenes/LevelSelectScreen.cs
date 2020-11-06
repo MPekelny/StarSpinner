616a740feec7182642ed6c8bb8077babe38bdcf6
Matthew Pekelny <mpekelny@gmail.com>
2020-11-06 16:31:29 -0500

    Added a number of puzzles and level selection pages.
    The levels d not have any sort of difficulty sorting or anything like that yet, right now they are just added in alphabetical order. But, there is enough now to be able to divide them into pages of puzzles, so the paging has been added now.

--- Assets/Scenes/LevelSelectScreen.cs
+++ Assets/Scenes/LevelSelectScreen.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LevelSelectScreen : MonoBehaviour
@@ -7,18 +8,19 @@ public class LevelSelectScreen : MonoBehaviour
 	[NonSerialized] public const string SCREEN_NAME = "LevelSelectScreen";
 
 	[SerializeField] private GameObject _levelSelectGrid = null;
+	[SerializeField] private GameObject _previousPageButton = null;
+	[SerializeField] private GameObject _nextPageButton = null;
+	[SerializeField] private int _numLevelsPerPage = 20;
 
+	private int _currentPageStartNumber = 0;
 	private List<LevelSelectButton> _buttons = new List<LevelSelectButton>();
+	private PuzzleData[] _allPuzzleDataReference;
 
 	public void Awake()
 	{
-		PuzzleData[] allPuzzleData = GameManager.Instance.GameDataReference.PuzzleDatas;
-		for (int i = 0; i < allPuzzleData.Length; i++)
-		{
-			LevelSelectButton button = GameManager.Instance.ObjectPoolManager.GetObjectFromPool("LevelSelectButton", _levelSelectGrid.transform).GetComponent<LevelSelectButton>();
-			button.Init(this, i);
-			_buttons.Add(button);
-		}
+		_currentPageStartNumber = 0;
+		_allPuzzleDataReference = GameManager.Instance.GameDataReference.PuzzleDatas;
+		ReloadPage();
 	}
 
 	public void LevelButtonPressed(int index)
@@ -27,6 +29,48 @@ public class LevelSelectScreen : MonoBehaviour
 
 		GameManager.Instance.SetActivePuzzleByIndex(index);
 		GameManager.Instance.ScreenTransitionManager.TransitionScreen(PuzzleScreen.SCREEN_NAME);
+	}
+
+	public void PreviousPageButtonPressed()
+	{
+		// Just in case, don't let the start number go below 0.
+		_currentPageStartNumber = Math.Max(0, _currentPageStartNumber - 20);
+		ReloadPage();
+	}
+
+	public void NextPageButtonPressed()
+	{
+		// Just in case, don't do anything (other than making sure the buttons are set correctly) if increasing the start point would go beyond the end of the level list.
+		if (_currentPageStartNumber + _numLevelsPerPage > _allPuzzleDataReference.Length)
+		{
+			SetPageButtonActiveness();
+		}
+		else
+		{
+			_currentPageStartNumber += _numLevelsPerPage;
+			ReloadPage();
+		}
+			
+	}
+
+	private void ReloadPage()
+	{
+		CleanUp();
+
+		for (int i = _currentPageStartNumber; i < _currentPageStartNumber + _numLevelsPerPage && i < _allPuzzleDataReference.Length; i++)
+		{
+			LevelSelectButton button = GameManager.Instance.ObjectPoolManager.GetObjectFromPool("LevelSelectButton", _levelSelectGrid.transform).GetComponent<LevelSelectButton>();
+			button.Init(this, i);
+			_buttons.Add(button);
+		}
+
+		SetPageButtonActiveness();
+	}
+
+	private void SetPageButtonActiveness()
+	{
+		_previousPageButton.SetActive(_currentPageStartNumber > 0);
+		_nextPageButton.SetActive(_currentPageStartNumber + _numLevelsPerPage < _allPuzzleDataReference.Length);
 	}
 
 	private void CleanUp()
