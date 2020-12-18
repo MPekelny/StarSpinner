39120f6e8d831a1bfcd7468453ff65610befefe6
Matthew Pekelny <mpekelny@gmail.com>
2020-12-18 14:17:54 -0500

    Save upgrading code is cleaned up.
    Much tidier than before. Now, as best I can tell, the save upgrading is complete.

--- Assets/Prefabs/Managers/SaveData/PuzzleSaveData.cs
+++ Assets/Prefabs/Managers/SaveData/PuzzleSaveData.cs
@@ -9,7 +9,6 @@ public class PuzzleSaveData
 	public class PuzzleStaticSaveData
 	{
 		private const string PUZZLE_VERSION_KEY = "puzzle_version";
-		private const string NUM_STARS_KEY = "num_stars";
 		private const string NUM_SPINNERS_KEY = "num_spinners";
 		private const string SPINNERS_LIST_KEY = "spinners_list";
 
@@ -17,6 +16,9 @@ public class PuzzleSaveData
 		public int NumSpinnersInPuzzle { get; set; } = -1;
 		public int PuzzleVersion { get; set; } = 0;
 
+		/// <summary>
+		/// Takes the save data and turns it into a json formatted string.
+		/// </summary>
 		public string WriteToJsonString()
 		{
 			JSONNode node = new JSONObject();
@@ -32,6 +34,9 @@ public class PuzzleSaveData
 			return builder.ToString();
 		}
 
+		/// <summary>
+		/// Creates the save data from a json formatted string.
+		/// </summary>
 		public void ReadFromJsonString(string json)
 		{
 			ResetData();
@@ -47,33 +52,124 @@ public class PuzzleSaveData
 			}
 		}
 
+		/// <summary>
+		/// Used as part of upgrading save data between versions of a puzzle, applies the changes that were in a single history data version to the save data.
+		/// </summary>
+		/// <param name="historyData">The history data to apply.</param>
+		public void ApplyPuzzleHistoryData(PuzzleData.HistoryData historyData)
+		{
+			for (int i = 0; i < historyData.NumStarsAdded; i++)
+			{
+				int rNum = UnityEngine.Random.Range(0, NumSpinnersInPuzzle);
+				SpinnersForStarsList.Add(rNum);
+			}
+
+			for (int i = 0; i < historyData.StarsDeleted.Length; i++)
+			{
+				int starToRemoveIndex = historyData.StarsDeleted[i];
+				if (starToRemoveIndex < SpinnersForStarsList.Count)
+				{
+					SpinnersForStarsList.RemoveAt(starToRemoveIndex);
+				}
+				else
+				{
+					Debug.LogError($"When applying puzzle history data to save data, tried to remove star at index {starToRemoveIndex} when there are only {SpinnersForStarsList.Count} stars in the save data.");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Given a number of stars the puzzle is meant to have, this method makes sure the number of stars it has in its data matches that number.
+		/// </summary>
+		/// <param name="correctNumStars">The number of stars there is meant to be.</param>
+		/// <returns>True if the data already had the correct number of stars, false otherwise.</returns>
+		public bool EnsureCorrectNumberOfStars(int correctNumStars)
+		{
+			if (correctNumStars < SpinnersForStarsList.Count)
+			{
+				Debug.LogWarning("The number of stars in the save data was more than the expected number of stars. Removing excess stars to match.");
+				SpinnersForStarsList.RemoveRange(correctNumStars, SpinnersForStarsList.Count - correctNumStars);
+				return false;
+			}
+			else if (correctNumStars > SpinnersForStarsList.Count)
+			{
+				Debug.LogWarning("The number of stars in the save data was less than the expected number of stars. Adding additional stars to match.");
+				int numToAdd = correctNumStars - SpinnersForStarsList.Count;
+				for (int i = 0; i < numToAdd; i++)
+				{
+					int rNum = UnityEngine.Random.Range(0, NumSpinnersInPuzzle);
+					SpinnersForStarsList.Add(rNum);
+				}
+
+				return false;
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Given a number of spinners the puzzle is meant to have, this method redistributes the stars so that there are stars on the correct number of spinners, if the data's number of spinners does not match
+		/// the expected amount.
+		/// </summary>
+		/// <param name="correctNumSpinners">The number of spinners there is meant to be.</param>
+		/// <returns>True, if the puzzle already contains the correct number of spinners, false otherwise.</returns>
+		public bool EnsureStarDistribution(int correctNumSpinners)
+		{
+			if (NumSpinnersInPuzzle > correctNumSpinners)
+			{
+				// In the case of there being more spinners in the data than expected, go through all the stars and any whose assigned spinner is beyond the expected range, randomly choose a valid spinner and reassign it to that.
+				Debug.Log("There are more spinners in the save data than expected, redistributing the stars on the extra spinners to the others.");
+				for (int i = 0; i < SpinnersForStarsList.Count; i++)
+				{
+					if (SpinnersForStarsList[i] >= correctNumSpinners)
+					{
+						int rNum = UnityEngine.Random.Range(0, correctNumSpinners);
+						SpinnersForStarsList[i] = rNum;
+					}
+				}
+
+				NumSpinnersInPuzzle = correctNumSpinners;
+				return false;
+			}
+			else if (NumSpinnersInPuzzle < correctNumSpinners)
+			{
+				// In the case of there being fewer spinners than expected, randomly pick an amount of stars that are assigned existing spinners and reassign them to the new spinner(s) so that the new spinners have a number of spinners roughly equal to the proportion of stars it should have.
+				// i.e. if there are 100 stars and went from 4 to 5 spinners, 20 stars are chosen to be set to the fifth spinner.
+				// This picking is weighted so that spinners with more stars are more likely to be picked from (and also prevents trying to pick a star from a spinner with no stars).
+				Debug.Log("There are fewer spinners in the save data than expected, redistributing some of the stars from each spinner to the new ones.");
+				List<int>[] indicesForSpinners = new List<int>[NumSpinnersInPuzzle];
+				for (int i = 0; i < indicesForSpinners.Length; i++)
+				{
+					indicesForSpinners[i] = new List<int>();
+				}
+
+				for (int i = 0; i < SpinnersForStarsList.Count; i++)
+				{
+					indicesForSpinners[SpinnersForStarsList[i]].Add(i);
+				}
+
+				int numStarsForSpinner = SpinnersForStarsList.Count / correctNumSpinners;
+				for (int i = NumSpinnersInPuzzle; i < correctNumSpinners; i++)
+				{
+					for (int j = 0; j < numStarsForSpinner; j++)
+					{
+						int spinnerToPullFrom = PickSpinnerToPullFromWeighted(indicesForSpinners);
+						int star = UnityEngine.Random.Range(0, indicesForSpinners[spinnerToPullFrom].Count);
+						SpinnersForStarsList[indicesForSpinners[spinnerToPullFrom][star]] = i;
+						indicesForSpinners[spinnerToPullFrom].RemoveAt(star);
+					}
+				}
+
+				NumSpinnersInPuzzle = correctNumSpinners;
+				return false;
+			}
+
+			return true;
+		}
+
 		public void AddSpinnerForStarItem(int spinnerIndex)
 		{
 			SpinnersForStarsList.Add(spinnerIndex);
-		}
-
-		public void RemoveSpinnerForStarItemAtIndex(int starIndex)
-		{
-			if (starIndex < SpinnersForStarsList.Count)
-			{
-				SpinnersForStarsList.RemoveAt(starIndex);
-			}
-			else
-			{
-				Debug.LogError("Tried to remove a star from the save data that is out of range of the list.");
-			}
-		}
-
-		public void RemoveSpinnerForStarItemsStartingAtIndex(int index)
-		{
-			if (index < SpinnersForStarsList.Count)
-			{
-				SpinnersForStarsList.RemoveRange(index, SpinnersForStarsList.Count - index);
-			}
-			else
-			{
-				Debug.LogError("Tried to remove stars from the save data that is starting out of range of the list.");
-			}
 		}
 
 		public void ResetData()
@@ -81,6 +177,34 @@ public class PuzzleSaveData
 			SpinnersForStarsList.Clear();
 			NumSpinnersInPuzzle = -1;
 			PuzzleVersion = 0;
+		}
+
+		/// <summary>
+		/// Used for the save data updating for when the number of spinners has increased from a previous version, it gets which of the spinners to pull a star from, weghted
+		/// so it is more likely to pull a star from a spinner with more stars.
+		/// </summary>
+		private int PickSpinnerToPullFromWeighted(List<int>[] starsSpread)
+		{
+			int[] weights = new int[starsSpread.Length];
+			int weightTotal = 0;
+			for (int i = 0; i < starsSpread.Length; i++)
+			{
+				weightTotal += starsSpread[i].Count;
+				weights[i] = weightTotal;
+			}
+
+			int rNum = UnityEngine.Random.Range(0, weightTotal);
+			for (int i = 0; i < weights.Length; i++)
+			{
+				if (rNum < weights[i])
+				{
+					return i;
+				}
+			}
+
+			// Shouldn't get here, but just in case, need to return something.
+			Debug.LogError($"When picking a weighted spinner, did not get one properly, so defaulting to returning 0. Number chosen: {rNum}, Weight total: {weightTotal}");
+			return 0;
 		}
 	}
 
@@ -94,6 +218,9 @@ public class PuzzleSaveData
 		public List<float> SpinnerTouchObjectRotations { get; set; } = new List<float>();
 		public int HintLockedSpinner { get; set; } = -1;
 
+		/// <summary>
+		/// Takes the save data and turns it into a json formatted string.
+		/// </summary>
 		public string WriteToJsonString()
 		{
 			JSONNode node = new JSONObject();
@@ -113,6 +240,9 @@ public class PuzzleSaveData
 			return builder.ToString();
 		}
 
+		/// <summary>
+		/// Creates the save data from a json formatted string.
+		/// </summary>
 		public void ReadFromJsonString(string json)
 		{
 			ResetData();
@@ -130,6 +260,38 @@ public class PuzzleSaveData
 			{
 				SpinnerTouchObjectRotations.Add(objectRotationsArray[i].AsFloat);
 			}
+		}
+
+		/// <summary>
+		/// Given a number of spinners, make sure the number of rotations in the save data match that number.
+		/// </summary>
+		/// <param name="correctNumSpinners"></param>
+		/// <returns></returns>
+		public bool EnsureCorrectNumberOfSpinners(int correctNumSpinners)
+		{
+			if (SpinnerRotations.Count > correctNumSpinners)
+			{
+				Debug.Log("There are more spinners in dynamic save data than expected, removing extras to match.");
+				SpinnerRotations.RemoveRange(correctNumSpinners, SpinnerRotations.Count - correctNumSpinners);
+				SpinnerTouchObjectRotations.RemoveRange(correctNumSpinners, SpinnerTouchObjectRotations.Count - correctNumSpinners);
+				return false;
+			}
+			else if (SpinnerRotations.Count < correctNumSpinners)
+			{
+				Debug.Log("There are fewer spinners in dynamic save data than expected, adding additional items to match.");
+				int numToAdd = correctNumSpinners - SpinnerRotations.Count;
+				for (int i = 0; i < numToAdd; i++)
+				{
+					float rRot = UnityEngine.Random.Range(0f, 360f);
+					float rObjRot = UnityEngine.Random.Range(0f, 360f);
+					SpinnerRotations.Add(rRot);
+					SpinnerTouchObjectRotations.Add(rObjRot);
+				}
+
+				return false;
+			}
+
+			return true;
 		}
 
 		public void ResetData()
--- Assets/Scenes/PuzzleScreen.cs
+++ Assets/Scenes/PuzzleScreen.cs
@@ -51,134 +51,9 @@ public class PuzzleScreen : MonoBehaviour
 
 		_puzzleSpinnersHelper.CreateSpinners(this, CheckSpinnerOverlap, CheckIfSolved, _gameData.SpinnerVisualDatas, _activePuzzle.NumSpinners);
 
-		TryLoadSaveData();
-	}
-
-	private void TryLoadSaveData()
-	{
 		if (GameManager.Instance.SaveDataManager.PuzzleStaticDataExistsForLevel(_activePuzzle.PuzzleUniqueId))
 		{
-			string staticSaveData = GameManager.Instance.SaveDataManager.GetPuzzleStaticDataForLevel(_activePuzzle.PuzzleUniqueId);
-			_saveDataHolder.StaticData.ReadFromJsonString(staticSaveData);
-			bool staticDataShouldBeUpdated = false;
-			// If for some reason the saved version is higher than the current version, just make the data fresh.
-			if (_saveDataHolder.StaticData.PuzzleVersion > _activePuzzle.CurrentVersionNumber)
-			{
-				Debug.LogError($"Save data for the puzzle {_activePuzzle.PuzzleUniqueId} exists, but for some reason the version number is greater than the current version. Generating the stars for the puzzle fresh.");
-				CreateStars();
-			}
-			else
-			{
-				int numSpinnersAtTime = _activePuzzle.HistoryDatas[_saveDataHolder.StaticData.PuzzleVersion].NumSpinners;
-
-				// 1. Check that the save data is updated to match up with the current version.
-				if (_saveDataHolder.StaticData.PuzzleVersion < _activePuzzle.CurrentVersionNumber)
-				{
-					Debug.Log($"Save data for {_activePuzzle.PuzzleUniqueId} is a version older than current, upgrading the save data to match the current version.");
-					for (int i = _saveDataHolder.StaticData.PuzzleVersion + 1; i <= _activePuzzle.CurrentVersionNumber; i++)
-					{
-						for (int j = 0; j < _activePuzzle.HistoryDatas[i].NumStarsAdded; j++)
-						{
-							int rNum = UnityEngine.Random.Range(0, numSpinnersAtTime);
-							_saveDataHolder.StaticData.AddSpinnerForStarItem(rNum);
-						}
-
-						for (int j = 0; j < _activePuzzle.HistoryDatas[i].StarsDeleted.Length; j++)
-						{
-							_saveDataHolder.StaticData.RemoveSpinnerForStarItemAtIndex(_activePuzzle.HistoryDatas[i].StarsDeleted[j]);
-						}
-					}
-
-					staticDataShouldBeUpdated = true;
-				}
-
-				// 2. Make sure that after any updating, there is the correct number of stars. Generally, this case should not happen, but it needs to be handled in case it does.
-				if (_saveDataHolder.StaticData.SpinnersForStarsList.Count != _activePuzzle.StarDatas.Length)
-				{
-					if (_saveDataHolder.StaticData.SpinnersForStarsList.Count > _activePuzzle.StarDatas.Length)
-					{
-						Debug.LogError($"Something happened when loading save data for puzzle {_activePuzzle.PuzzleUniqueId}, there were more stars in the save data than there are in the puzzle. Removing excess stars.");
-						_saveDataHolder.StaticData.RemoveSpinnerForStarItemsStartingAtIndex(_activePuzzle.StarDatas.Length);
-					}
-					else
-					{
-						Debug.LogError($"Something happened when loading save data for puzzle {_activePuzzle.PuzzleUniqueId}, there were fewer stars in the save data than there are in the puzzle. Adding additional stars.");
-						int numToAdd = _activePuzzle.StarDatas.Length - _saveDataHolder.StaticData.SpinnersForStarsList.Count;
-						for (int i = 0; i < numToAdd; i++)
-						{
-							int rNum = UnityEngine.Random.Range(0, numSpinnersAtTime + 1);
-							_saveDataHolder.StaticData.AddSpinnerForStarItem(rNum);
-						}
-					}
-
-					staticDataShouldBeUpdated = true;
-				}
-
-				// 3. Make sure the stars are distributed among the correct number of spinners.
-				if (numSpinnersAtTime != _activePuzzle.NumSpinners)
-				{
-					if (numSpinnersAtTime > _activePuzzle.NumSpinners)
-					{
-						Debug.Log($"When upgrading puzzle save data for {_activePuzzle.PuzzleUniqueId}, the save data has more spinners than the current version. Redistributing any stars on the extra spinners to the valid ones.");
-
-						// If more, simply take all that are set to spinners equal to or higher than the numSpinners, and set their spinners to a valid one.
-						for (int i = 0; i < _saveDataHolder.StaticData.SpinnersForStarsList.Count; i++)
-						{
-							if (_saveDataHolder.StaticData.SpinnersForStarsList[i] >= _activePuzzle.NumSpinners)
-							{
-								int rNum = UnityEngine.Random.Range(0, _activePuzzle.NumSpinners);
-								_saveDataHolder.StaticData.SpinnersForStarsList[i] = rNum;
-							}
-						}
-					}
-					else
-					{
-						Debug.Log($"When upgrading puzzle save data for {_activePuzzle.PuzzleUniqueId}, the save data has fewer spinners than the current version. Redistributing some stars from current spinners onto the new ones.");
-						// If less, take a number of stars from the existing ones and asign them to the higher ones.
-						// To make it simpler to reassign, make a list of all indices for each current spinner.
-						List<int>[] indicesForSpinners = new List<int>[numSpinnersAtTime];
-						for (int i = 0; i < indicesForSpinners.Length; i++)
-						{
-							indicesForSpinners[i] = new List<int>();
-						}
-
-						for (int i = 0; i < _saveDataHolder.StaticData.SpinnersForStarsList.Count; i++)
-						{
-							indicesForSpinners[_saveDataHolder.StaticData.SpinnersForStarsList[i]].Add(i);
-						}
-
-						int numStarsForSpinner = _saveDataHolder.StaticData.SpinnersForStarsList.Count / _activePuzzle.NumSpinners;
-						for (int i = numSpinnersAtTime; i < _activePuzzle.NumSpinners; i++)
-						{
-							for (int j = 0; j < numStarsForSpinner; j++)
-							{
-								int spinnerToPullFrom = PickSpinnerToPullFromWeighted(indicesForSpinners);
-								int star = UnityEngine.Random.Range(0, indicesForSpinners[spinnerToPullFrom].Count);
-								_saveDataHolder.StaticData.SpinnersForStarsList[indicesForSpinners[spinnerToPullFrom][star]] = i;
-								indicesForSpinners[spinnerToPullFrom].RemoveAt(star);
-							}
-						}
-					}
-
-					staticDataShouldBeUpdated = true;
-				}
-
-
-				// 4. Create the stars using the save data.
-				if (!CreateStarsFromSaveData())
-				{
-					// The create stars from save data should not fail at this point, but as a final just in case, create them fresh if it does fail for some reason.
-					Debug.LogError("Somehow, after all the save data updating, it still failed to create the stars from the save data. So, creating the save data fresh.");
-					CreateStars();
-				}
-
-				// 5. If marked for updating, update the static save data.
-				if (staticDataShouldBeUpdated)
-				{
-					_saveDataHolder.StaticData.PuzzleVersion = _activePuzzle.CurrentVersionNumber;
-					GameManager.Instance.SaveDataManager.SavePuzzleStaticDataForLevel(_activePuzzle.PuzzleUniqueId, _saveDataHolder.StaticData.WriteToJsonString());
-				}
-			}
+			ReadInStaticSaveData();
 		}
 		else
 		{
@@ -187,64 +62,7 @@ public class PuzzleScreen : MonoBehaviour
 
 		if (GameManager.Instance.SaveDataManager.PuzzleDynamicDataExistsForLevel(_activePuzzle.PuzzleUniqueId))
 		{
-			string dynamicSaveData = GameManager.Instance.SaveDataManager.GetPuzzleDynamicDataForLevel(_activePuzzle.PuzzleUniqueId);
-			_saveDataHolder.DynamicData.ReadFromJsonString(dynamicSaveData);
-			bool dynamicDataShouldBeUpdated = false;
-			// 6. If the number of spinners in the save data is not the same as the puzzle's current version, adjust it.
-			if (_saveDataHolder.DynamicData.SpinnerRotations.Count != _activePuzzle.NumSpinners)
-			{
-				if (_saveDataHolder.DynamicData.SpinnerRotations.Count > _activePuzzle.NumSpinners)
-				{
-					Debug.Log($"While loading dynamic save data for the puzzle {_activePuzzle.PuzzleUniqueId}, the data had more spinners than the current version, removing the extras from thge dynamic data.");
-					_saveDataHolder.DynamicData.SpinnerRotations.RemoveRange(_activePuzzle.NumSpinners, _saveDataHolder.DynamicData.SpinnerRotations.Count - _activePuzzle.NumSpinners);
-					_saveDataHolder.DynamicData.SpinnerTouchObjectRotations.RemoveRange(_activePuzzle.NumSpinners, _saveDataHolder.DynamicData.SpinnerTouchObjectRotations.Count - _activePuzzle.NumSpinners);
-				}
-				else
-				{
-					Debug.Log($"While loading dynamic save data for the puzzle {_activePuzzle.PuzzleUniqueId}, the data had fewer spinners than the current version, adding additional equal to the difference.");
-					int numToAdd = _activePuzzle.NumSpinners - _saveDataHolder.DynamicData.SpinnerRotations.Count;
-					for (int i = 0; i < numToAdd; i++)
-					{
-						float rRot = UnityEngine.Random.Range(0f, 360f);
-						float rObjRot = UnityEngine.Random.Range(0f, 360f);
-						_saveDataHolder.DynamicData.SpinnerRotations.Add(rRot);
-						_saveDataHolder.DynamicData.SpinnerTouchObjectRotations.Add(rObjRot);
-					}
-				}
-
-				dynamicDataShouldBeUpdated = true;
-			}
-
-			// 7. If there is a hint locked spinner and it is now greater than the number of spinners, randomly rechoose it.
-			if (_saveDataHolder.DynamicData.HintLockedSpinner >= _activePuzzle.NumSpinners)
-			{
-				Debug.Log($"When loading the dynamic save data for the puzzle {_activePuzzle.PuzzleUniqueId}, the hint locked spinner is greater than the number of spinners, so randomly rechoosing it.");
-				_saveDataHolder.DynamicData.HintLockedSpinner = UnityEngine.Random.Range(0, _activePuzzle.NumSpinners);
-				dynamicDataShouldBeUpdated = true;
-			}
-
-			// 8. Set the spinner rotations based on save data.
-			if (!_puzzleSpinnersHelper.SetRotationsForSpinners(_saveDataHolder.DynamicData.SpinnerRotations, _saveDataHolder.DynamicData.SpinnerTouchObjectRotations))
-			{
-				// This should not fail at this point, but just in case it needs to be handled.
-				Debug.LogError($"When loading the dynamic save data for the puzzle {_activePuzzle.PuzzleUniqueId}, setting the spinners with the save data failed, so randomly spinning them.");
-				_puzzleSpinnersHelper.RandomSpinSpinners();
-				dynamicDataShouldBeUpdated = true;
-			}
-
-			// 9. Make sure the hint locked spinner is set correctly.
-			if (_saveDataHolder.DynamicData.HintLockedSpinner > -1)
-			{
-				_puzzleSpinnersHelper.HintLockSpinner(_saveDataHolder.DynamicData.HintLockedSpinner);
-				_hintButton.gameObject.SetActive(false);
-			}
-
-			// 10. If marked for updating, make sure overlaps are resolved and update dynamic save data.
-			if (dynamicDataShouldBeUpdated)
-			{
-				CheckSpinnerOverlap(); 
-				UpdateDynamicSaveData();
-			}
+			ReadInDynamicSaveData();
 		}
 		else
 		{
@@ -255,32 +73,104 @@ public class PuzzleScreen : MonoBehaviour
 		}
 	}
 
-	/// <summary>
-	/// Used for the save data updating for when the number of spinners has increased from a previous version, it gets which of the spinners to pull a star from, weghted
-	/// so it is more likely to pull a star from a spinner with more stars.
-	/// </summary>
-	private int PickSpinnerToPullFromWeighted(List<int>[] starsSpread)
+	private void ReadInStaticSaveData()
 	{
-		int[] weights = new int[starsSpread.Length];
-		int weightTotal = 0;
-		for (int i = 0; i < starsSpread.Length; i++)
+		string staticSaveData = GameManager.Instance.SaveDataManager.GetPuzzleStaticDataForLevel(_activePuzzle.PuzzleUniqueId);
+		_saveDataHolder.StaticData.ReadFromJsonString(staticSaveData);
+		bool staticDataShouldBeUpdated = false;
+		// If for some reason the saved version is higher than the current version, just make the data fresh.
+		if (_saveDataHolder.StaticData.PuzzleVersion > _activePuzzle.CurrentVersionNumber)
 		{
-			weightTotal += starsSpread[i].Count;
-			weights[i] = weightTotal;
+			Debug.LogError($"Save data for the puzzle {_activePuzzle.PuzzleUniqueId} exists, but for some reason the version number is greater than the current version. Generating the stars for the puzzle fresh.");
+			CreateStars();
 		}
-
-		int rNum = UnityEngine.Random.Range(0, weightTotal);
-		for (int i = 0; i < weights.Length; i++)
+		else
 		{
-			if (rNum < weights[i])
-			{ 
-				return i; 
+			// 1. Check that the save data is updated to match up with the current version.
+			if (_saveDataHolder.StaticData.PuzzleVersion < _activePuzzle.CurrentVersionNumber)
+			{
+				Debug.Log($"Save data for {_activePuzzle.PuzzleUniqueId} is a version older than current, upgrading the save data to match the current version.");
+				for (int i = _saveDataHolder.StaticData.PuzzleVersion + 1; i <= _activePuzzle.CurrentVersionNumber; i++)
+				{
+					_saveDataHolder.StaticData.ApplyPuzzleHistoryData(_activePuzzle.HistoryDatas[i]);
+				}
+
+				staticDataShouldBeUpdated = true;
+			}
+
+			// 2. Make sure that after any updating, there is the correct number of stars. Generally, this case should not happen, but it needs to be handled in case it does.
+			if (!_saveDataHolder.StaticData.EnsureCorrectNumberOfStars(_activePuzzle.StarDatas.Length))
+			{
+				Debug.LogError($"After upgrade static save data for {_activePuzzle.PuzzleUniqueId}, the number of stars in the data did not match the puzzle data, so an additional adjustment had to be made.");
+				staticDataShouldBeUpdated = true;
+			}
+
+			// 3. Make sure the stars are distributed among the correct number of spinners.
+			if (!_saveDataHolder.StaticData.EnsureStarDistribution(_activePuzzle.NumSpinners))
+			{
+				Debug.Log($"Number of spinners in static save data for {_activePuzzle.PuzzleUniqueId} was different than the puzzle data, the stars were redistributed to fit the puzzle data.");
+				staticDataShouldBeUpdated = true;
+			}
+
+			// 4. Create the stars using the save data.
+			if (!CreateStarsFromSaveData())
+			{
+				// The create stars from save data should not fail at this point, but as a final just in case, create them fresh if it does fail for some reason.
+				Debug.LogError("Somehow, after all the save data updating, it still failed to create the stars from the save data. So, creating the save data fresh.");
+				CreateStars();
+			}
+
+			// 5. If marked for updating, update the static save data.
+			if (staticDataShouldBeUpdated)
+			{
+				_saveDataHolder.StaticData.PuzzleVersion = _activePuzzle.CurrentVersionNumber;
+				GameManager.Instance.SaveDataManager.SavePuzzleStaticDataForLevel(_activePuzzle.PuzzleUniqueId, _saveDataHolder.StaticData.WriteToJsonString());
 			}
 		}
+	}
 
-		// Shouldn't get here, but just in case, need to return something.
-		Debug.LogError($"When picking a weighted spinner, did not get one properly, so defaulting to returning 0. Number chosen: {rNum}, Weight total: {weightTotal}");
-		return 0;
+	private void ReadInDynamicSaveData()
+	{
+		string dynamicSaveData = GameManager.Instance.SaveDataManager.GetPuzzleDynamicDataForLevel(_activePuzzle.PuzzleUniqueId);
+		_saveDataHolder.DynamicData.ReadFromJsonString(dynamicSaveData);
+		bool dynamicDataShouldBeUpdated = false;
+		// 6. If the number of spinners in the save data is not the same as the puzzle's current version, adjust it.
+		if (!_saveDataHolder.DynamicData.EnsureCorrectNumberOfSpinners(_activePuzzle.NumSpinners))
+		{
+			Debug.Log($"When loading the dynamic save data for the puzzle {_activePuzzle.PuzzleUniqueId}, the number of saved spinners was different than the puzzle's data, so it was adjusted to match.");
+			dynamicDataShouldBeUpdated = true;
+		}
+
+		// 7. If there is a hint locked spinner and it is now greater than the number of spinners, randomly rechoose it.
+		if (_saveDataHolder.DynamicData.HintLockedSpinner >= _activePuzzle.NumSpinners)
+		{
+			Debug.Log($"When loading the dynamic save data for the puzzle {_activePuzzle.PuzzleUniqueId}, the hint locked spinner is greater than the number of spinners, so randomly rechoosing it.");
+			_saveDataHolder.DynamicData.HintLockedSpinner = UnityEngine.Random.Range(0, _activePuzzle.NumSpinners);
+			dynamicDataShouldBeUpdated = true;
+		}
+
+		// 8. Set the spinner rotations based on save data.
+		if (!_puzzleSpinnersHelper.SetRotationsForSpinners(_saveDataHolder.DynamicData.SpinnerRotations, _saveDataHolder.DynamicData.SpinnerTouchObjectRotations))
+		{
+			// This should not fail at this point, but just in case it needs to be handled.
+			Debug.LogError($"When loading the dynamic save data for the puzzle {_activePuzzle.PuzzleUniqueId}, setting the spinners with the save data failed, so randomly spinning them.");
+			_puzzleSpinnersHelper.RandomSpinSpinners();
+			dynamicDataShouldBeUpdated = true;
+		}
+
+		// 9. Make sure the hint locked spinner is set correctly.
+		if (_saveDataHolder.DynamicData.HintLockedSpinner > -1)
+		{
+			_puzzleSpinnersHelper.HintLockSpinner(_saveDataHolder.DynamicData.HintLockedSpinner);
+			_hintButton.gameObject.SetActive(false);
+		}
+
+		// 10. If marked for updating, make sure overlaps are resolved and update dynamic save data.
+		if (dynamicDataShouldBeUpdated)
+		{
+			CheckSpinnerOverlap();
+			UpdateDynamicSaveData();
+		}
 	}
 
 	/// <summary>
